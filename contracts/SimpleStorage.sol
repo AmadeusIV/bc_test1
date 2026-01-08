53de4d3a804607f7c973ae1f56c9ee9b078f04ab
AmadeusIV <quanhm0412@gmail.com>
2026-01-08 12:46:42 +0700

    Initial commit: Add Truffle smart contracts and migrations

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage {
+    uint256 public storedData;
+
+    constructor(uint256 _initialValue) {
+        storedData = _initialValue;
+    }
+
+    function set(uint256 _value) public {
+        storedData = _value;
+    }
+
+    function get() public view returns (uint256) {
+        return storedData;
+    }
+}
+
